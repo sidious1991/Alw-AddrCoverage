e85f4db24785c8a2bc3e39d0021ae622fbebc3cb
sidious1991 <agostinistefano1991@gmail.com>
2017-10-26 16:21:41 +0200

    Start GeomLib.

--- Alw-AddrCoverage/src/main/java/com/stefano/GeomLib.java
+++ Alw-AddrCoverage/src/main/java/com/stefano/GeomLib.java
@@ -7,4 +7,31 @@ import org.postgis.*;
 
 public class GeomLib {
 
+	public static ResultSet getIntersectingLines(PGgeometry street, java.sql.Connection conn, PreparedStatement s) {
+		/**
+		 * This method returns a ResultSet of all the lines in the planet_osm_line
+		 * table, whose geometric attribute (way) intersects the geometry street
+		 **/
+
+		ResultSet r = null;
+
+		try {
+
+			((org.postgresql.PGConnection) conn).addDataType("geometry", Class.forName("org.postgis.PGgeometry"));
+			((org.postgresql.PGConnection) conn).addDataType("box3d", Class.forName("org.postgis.PGbox3d"));
+
+			s = conn.prepareStatement(
+					"select l.name from planet_osm_line l where (ST_Intersects(l.way, ?) = true and l.name is not null)");
+			s.setObject(1, street);
+			r = s.executeQuery();
+
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return r;
+	}
+
 }
--- Alw-AddrCoverage/src/main/java/com/stefano/Test.java
+++ Alw-AddrCoverage/src/main/java/com/stefano/Test.java
@@ -6,48 +6,50 @@ import java.lang.*;
 import org.postgis.*;
 
 public class Test {
-	
+
 	public static void main(String args[]) {
-		
+
 		java.sql.Connection conn;
-		
-		System.out.println("Hi Paolo!\n");
-		
+
 		try {
-		    /*
-		    * Load the JDBC driver and establish a connection.
-		    */
-		    Class.forName("org.postgresql.Driver");
-		    String url = "jdbc:postgresql://localhost:5432/avezzano";
-		    conn = DriverManager.getConnection(url, "postgres", "26042015");
-		    
-		    ((org.postgresql.PGConnection)conn).addDataType("geometry",Class.forName("org.postgis.PGgeometry"));
-		    ((org.postgresql.PGConnection)conn).addDataType("box3d",Class.forName("org.postgis.PGbox3d"));
-		    
-		    Statement s = conn.createStatement();
-		    ResultSet r = s.executeQuery("select name, \"addr:housenumber\", ST_AsText(way) from planet_osm_point where \"addr:housenumber\" is not null");
-		    
-		    while( r.next() ) {
-		      
-		        String name = r.getString(1);
-		        String addr = r.getString(2);
-		        String geom_text = r.getString(3);
-		        //PGgeometry geom_way = (PGgeometry)r.getObject(2);
-		        
-		        System.out.println("Name: " + name);
-		        System.out.println("Address: " + addr);
-		        System.out.println("Geom as a text: " + geom_text);
-		    }
-		    
-		    
-		    s.close();
-		    conn.close();
+
+			Class.forName("org.postgresql.Driver");
+			String url = "jdbc:postgresql://localhost:5432/avezzano";
+			conn = DriverManager.getConnection(url, "postgres", "26042015");
+
+			((org.postgresql.PGConnection) conn).addDataType("geometry", Class.forName("org.postgis.PGgeometry"));
+			((org.postgresql.PGConnection) conn).addDataType("box3d", Class.forName("org.postgis.PGbox3d"));
+
+			String name = "Via Sergio Cataldi";
+			PreparedStatement s = conn.prepareStatement("select way from planet_osm_line where name = ?");
+			s.setString(1, name);
+			ResultSet r = s.executeQuery();
+			ResultSet rs = null;
+
+			PGgeometry street;
+
+			while (r.next()) {
+
+				street = (PGgeometry) r.getObject(1);
+				rs = GeomLib.getIntersectingLines(street, conn, s);
+
+				if(rs!=null) {
+					while(rs.next()) {
+						System.out.println(rs.getString(1));
+					}
+				}
+
+			}
+
+			s.close();
+			conn.close();
+
 		}
-		
-		catch ( Exception e ){
+
+		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-		
+
 }
